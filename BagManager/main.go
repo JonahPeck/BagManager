1f99cb633c5d3bd0860bf74833fa8a31c694ccb2
JonahPeck <PeckJonahC@gmail.com>
2023-10-14 11:39:23 -0400

    golang post

--- BagManager/main.go
+++ BagManager/main.go
@@ -4,13 +4,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"time"
 )
+
+type Post struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 func main() {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World I like pizza",
+			"message": "Hello World",
 		})
 	})
 
