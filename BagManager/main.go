3c481a1cb74f67c59c715f657f670b2d4534a12d
JonahPeck <PeckJonahC@gmail.com>
2023-10-14 11:58:12 -0400

    models

--- BagManager/main.go
+++ BagManager/main.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"net/http"
+	"JonahPeck/BagManager/BagManager/models"
 
 	"github.com/gin-gonic/gin"
-	"time"
 )
-
-type Post struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
-}
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World",
-		})
-	})
+	models.ConnectDatabase()
 
 	router.Run("localhost:8080")
 }
