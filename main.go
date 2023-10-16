3b4f777a713e887e906452479cc8a8b6ae3645af
JonahPeck <PeckJonahC@gmail.com>
2023-10-15 21:02:30 -0400

    trouble with getting valid post

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 
 	router.POST("/posts", controllers.CreatePost)
 	router.GET("/posts", controllers.FindPosts)
+	router.GET("posts/:id", controllers.FindPost)
 
 	router.Run("localhost:8080")
 }
