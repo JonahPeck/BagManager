9d94ff5b4dfbf691808c489d07e8bd512c736b47
JonahPeck <PeckJonahC@gmail.com>
2023-10-16 20:51:16 -0400

    patch'

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 	router.POST("/posts", controllers.CreatePost)
 	router.GET("/posts", controllers.FindPosts)
 	router.GET("posts/:id", controllers.FindPost)
+	router.PATCH("/posts/:id", controllers.UpdatePost)
 
 	router.Run("localhost:8080")
 }
